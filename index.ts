79535301b331089b17bf39063e298cc3545da2c5
yashiroryo <>
2022-06-19 01:31:57 +0900

    fix: port 番号

--- index.ts
+++ index.ts
@@ -8,9 +8,9 @@ import restRouter from "./router/restRouter";
 import userRouter from "./router/userRouter";
 
 const app: Application = express();
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 5000;
 
-app.set("port", process.env.PORT || 3030);
+app.set("port", process.env.PORT || 5000);
 
 const server = https.createServer(
   {
